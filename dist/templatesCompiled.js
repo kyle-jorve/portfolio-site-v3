f9fa72e58284a48cc346ece0f5d9bf491c72a2dd
Kyle Jorve <kyle.jorve@gmail.com>
2021-10-08 17:22:37 -0700

    footer social icon grid is dynamic now

--- dist/templatesCompiled.js
+++ dist/templatesCompiled.js
@@ -8,24 +8,28 @@ templates['components/footer'] = template({"1":function(container,depth0,helpers
         return undefined
     };
 
-  return "            "
-    + ((stack1 = (lookupProperty(helpers,"printSocialIcon")||(depth0 && lookupProperty(depth0,"printSocialIcon"))||container.hooks.helperMissing).call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"printSocialIcon","hash":{},"data":data,"loc":{"start":{"line":4,"column":12},"end":{"line":4,"column":38}}})) != null ? stack1 : "")
+  return "			"
+    + ((stack1 = (lookupProperty(helpers,"printSocialIcon")||(depth0 && lookupProperty(depth0,"printSocialIcon"))||container.hooks.helperMissing).call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"printSocialIcon","hash":{},"data":data,"loc":{"start":{"line":7,"column":3},"end":{"line":7,"column":29}}})) != null ? stack1 : "")
     + "\n";
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), lookupProperty = container.lookupProperty || function(parent, propertyName) {
+    var stack1, helper, alias1=container.lambda, alias2=container.escapeExpression, alias3=depth0 != null ? depth0 : (container.nullContext || {}), lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
           return parent[propertyName];
         }
         return undefined
     };
 
-  return "<div class=\"footer__icons\">\n    <div class=\"social social--footer social--standard\">\n"
-    + ((stack1 = lookupProperty(helpers,"each").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"socialIcons") : depth0)) != null ? lookupProperty(stack1,"standard") : stack1),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":3,"column":8},"end":{"line":5,"column":17}}})) != null ? stack1 : "")
-    + "    </div>\n\n    <div class=\"social social--footer social--commerce\">\n"
-    + ((stack1 = lookupProperty(helpers,"each").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"socialIcons") : depth0)) != null ? lookupProperty(stack1,"commerce") : stack1),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":9,"column":8},"end":{"line":11,"column":17}}})) != null ? stack1 : "")
-    + "    </div>\n</div>\n\n<div class=\"footer__copyright\">\n    <div class=\"wrapper wrapper--content\">\n        "
-    + ((stack1 = ((helper = (helper = lookupProperty(helpers,"copyright") || (depth0 != null ? lookupProperty(depth0,"copyright") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(alias1,{"name":"copyright","hash":{},"data":data,"loc":{"start":{"line":17,"column":8},"end":{"line":17,"column":23}}}) : helper))) != null ? stack1 : "")
-    + "\n    </div>\n</div>";
+  return "<div\n	class='footer__icons'\n	style='grid-template: auto / "
+    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"socialIcons") : depth0)) != null ? lookupProperty(stack1,"standard") : stack1)) != null ? lookupProperty(stack1,"length") : stack1), depth0))
+    + "fr "
+    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"socialIcons") : depth0)) != null ? lookupProperty(stack1,"commerce") : stack1)) != null ? lookupProperty(stack1,"length") : stack1), depth0))
+    + "fr;'\n>\n	<div class='social social--footer social--standard'>\n"
+    + ((stack1 = lookupProperty(helpers,"each").call(alias3,((stack1 = (depth0 != null ? lookupProperty(depth0,"socialIcons") : depth0)) != null ? lookupProperty(stack1,"standard") : stack1),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":6,"column":2},"end":{"line":8,"column":11}}})) != null ? stack1 : "")
+    + "	</div>\n\n	<div class='social social--footer social--commerce'>\n"
+    + ((stack1 = lookupProperty(helpers,"each").call(alias3,((stack1 = (depth0 != null ? lookupProperty(depth0,"socialIcons") : depth0)) != null ? lookupProperty(stack1,"commerce") : stack1),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":12,"column":2},"end":{"line":14,"column":11}}})) != null ? stack1 : "")
+    + "	</div>\n</div>\n\n<div class='footer__copyright'>\n	<div class='wrapper wrapper--content'>\n		"
+    + ((stack1 = ((helper = (helper = lookupProperty(helpers,"copyright") || (depth0 != null ? lookupProperty(depth0,"copyright") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(alias3,{"name":"copyright","hash":{},"data":data,"loc":{"start":{"line":20,"column":2},"end":{"line":20,"column":17}}}) : helper))) != null ? stack1 : "")
+    + "\n	</div>\n</div>";
 },"useData":true});
 templates['components/head'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
